02d55ae8fc1dc97db37779e3fe26c9754089e9e6
andrew.a <Jeffl@Andrews-MacBook-Pro.local>
2015-10-25 19:09:17 +0000

    Front end created. Requests page almost done

---
+++ request-detail.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Kingdom Christian Fellowship | Events</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap-responsive.css" />
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+	
+<!-- 	Web Browser thumbnail image -->
+    <link rel="shortcut icon" href="#">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>  	
+  	<div class="container entire-nav">
+        <nav class="navbar navbar-fixed-top" role="navigation">
+        	<div class="col-md-12 col-sm-12 real-nav">
+	        	<div class="navbar-header">
+				    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+				      <span class="sr-only">Toggle navigation</span>
+				    </button>
+					<a class="navbar-brand logo" href="index.html"><img src="images/logo.png" alt="Logo" class="logo-img img-responsive"/></a>
+				</div>
+				<div class="collapse navbar-collapse pull-right " id="navbar-collapse-01">
+					<ul class="nav navbar-nav menuitem">           
+						<li><a href="index.html"><p>HOME</p></a></li>
+						<li><a href="about.html"><p>ABOUT</p></a></li>
+						<li><a href="messages.html"><p>MESSAGES</p></a></li>
+						<li><a href="family.html"><p>FAMILY</p></a></li>
+						<li class="active"><a href="events.html"><p>EVENTS</p></a></li>
+						<li><a href="contact.html"><p>CONTACT</p></a></li>
+					</ul>				
+				</div><!-- /.navbar-collapse -->
+	
+        	</div>
+		</nav>
+    </div>
+
+  	<section class="slide-minus events-bg">
+  		<div class="container">
+	  		<div class="row">
+	  		</div>
+  		</div>
+  	</section>
+  	
+  	
+<!--   	<div class="hr-divider"></div> -->
+  	
+  	<section class="writing">
+         <div class="container">
+            <div class="col-md-8 col-md-offset-2 tab-menu">
+                <p class="focused">Upcoming Events <span class="navbar-new">12</span></p>
+                <p>Weekly Events</p>
+            </div>
+            
+             <div class="col-md-8 col-md-offset-2">
+               
+                <div class="col-md-12 single-event">
+                    <div class="col-md-2 event-date">
+		    			<h3>23rd</h3>
+		    			<h6>AUG</h6>
+	    			</div>
+	    			
+	    			<div class="col-md-10 event-details">
+	    				<p>The Holy Ghost Conference</p>
+		    			<img src="#" alt="" class="img-resposive news-image" />
+		    			<br />
+		    			<small>
+			    			Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse... <a href="news-item.html"> See More</a>
+		    			</small>
+	    			</div>
+                </div>
+                
+                
+                <div class="col-md-12 single-event">
+                    <div class="col-md-2 event-date">
+		    			<h3>13th</h3>
+		    			<h6>DEC</h6>
+	    			</div>
+	    			
+	    			<div class="col-md-10 event-details">
+	    				<p>Worship Night</p>
+		    			<img src="#" alt="" class="img-resposive news-image" />
+		    			<br />
+		    			<small>
+			    			Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse... <a href="news-item.html"> See More</a>
+		    			</small>
+	    			</div>
+                </div>
+                
+                  
+            </div>
+         </div>
+  	    
+  	</section>
+  	
+  	<footer>
+	  	<div class="container">
+	  		<div class="row">
+	  		    <div class="col-md-12">
+                    <div class="col-md-2">
+                        <img src="images/logo.jpg" alt="" class="img-responsive">
+                    </div>
+                    
+                    <div class="col-md-3 col-xs-6">
+                        <ul class="footer-list">
+		  				<li><a href="#">PHOTOGRAPHY</a></li>
+		  				<li><a href="#">PORTRAITS</a></li>
+		  				<li><a href="#">LANDSCAPE</a></li>
+		  				<li><a href="#">WEDDINGS</a></li>
+		  				<li><a href="#">THEMED</a></li>
+		  			</ul>
+                    </div>
+                    
+                    <div class="col-md-3 col-xs-6">
+                        <ul class="footer-list">
+                            <li><a href="#">PHOTOGRAPHY</a></li>
+                            <li><a href="#">PORTRAITS</a></li>
+                            <li><a href="#">LANDSCAPE</a></li>
+                            <li><a href="#">WEDDINGS</a></li>
+                            <li><a href="#">THEMED</a></li>
+                        </ul>
+                    </div>
+                    
+                    <div class="col-md-3">
+                        <form action="" class="sb-form">
+                            <label for="">Subscribe</label>
+                            <input type="text" class="form-control input-sm" placeholder="Enter email and tap ENTER" />
+                        </form>
+                    </div>
+                    
+                    <div class="col-md-1 social-icons">
+                        <a href="#"><img src="images/icons/facebook.png" alt="" class="img-responsive"></a>
+                        <a href="#"><img src="images/icons/twitter.png" alt="" class="img-responsive"></a>
+                        <a href="#"><img src="images/icons/instagram.png" alt="" class="img-responsive"></a>
+                        <a href="#"><img src="images/icons/facebook.png" alt="" class="img-responsive"></a>
+                    </div>
+	  		            
+	  		    </div>
+	  		    
+	  			
+	  		
+	  			<div class="col-sm-12 col-md-12 copyright">
+	  				<p>&copy; Copyright 2015. Kingdom Christian Fellowship. All rights reserved</p>
+	  			</div>			
+	  		</div>
+	  	</div>
+  	</footer>
+  	
+   
+   
+
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-switch.js"></script>
+    <script src="js/flatui-checkbox.js"></script>
+    <script src="js/flatui-radio.js"></script>
+    <script src="js/jquery.tagsinput.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+  </body>
+</html>
